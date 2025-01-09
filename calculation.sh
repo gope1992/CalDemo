676abc913f4b6840f0907895d449cd6d5f153ef6
Ubuntu <ubuntu@ip-172-31-25-107.ec2.internal>
2025-01-09 04:46:23 +0000

    my second comment

--- calculation.sh
+++ calculation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #simple its calculator program
 echo "hello displaying the given value as well"
-#chenged here
-#extra part of that
 a=1+2
-#new added lines:wq!
+#internally also added the values as well
